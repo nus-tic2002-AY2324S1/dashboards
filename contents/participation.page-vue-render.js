84882a9bc7df0a27694e83bccd1a30ea0a969db0
damithc <damith@github>
2023-09-25 21:43:02 +0800

    Site Update. [skip ci]

--- contents/participation.page-vue-render.js
+++ contents/participation.page-vue-render.js
@@ -161,6 +161,6 @@ with(this){return _c('td',[_c('div',[_c('p',[_v("A---5469R")])])])}
 with(this){return _c('td',[_c('div',[_c('p',[_c('span',{staticClass:"badge bg-success me-1"},[_v("1")]),_c('span',{staticClass:"badge bg-success me-1"},[_v("2")]),_c('span',{staticClass:"badge bg-success me-1"},[_v("3")]),_c('span',{staticClass:"badge bg-success me-1"},[_v("4")]),_v(" → "),_c('strong',[_v("4")])])])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.1.0")]),_v(", generated on Mon, 25 Sept 2023, 11:36:51 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.1.0")]),_v(", generated on Mon, 25 Sept 2023, 21:42:55 GMT+8]")])])])}
 }];
   
